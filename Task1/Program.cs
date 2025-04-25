303e33acd9a7001ccfb59f49bcdd656b8fa589b5
MinoddeinEz <kamazitun.jaykehd@gmail.com>
2025-04-26 00:37:05 +0300

    feat(task1): init project

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System.Text;
 
 Console.WriteLine(Solution.Convert("aaabbcccdde"));
+Console.WriteLine(Solution.Deconvert("a3b2c3d2e"));
 Console.WriteLine(Solution.Convert("aaaaaa"));
+Console.WriteLine(Solution.Deconvert("a6"));
 Console.WriteLine(Solution.Convert("aaabbbbb"));
+Console.WriteLine(Solution.Deconvert("a3b5"));
 Console.WriteLine(Solution.Convert("ab"));
+Console.WriteLine(Solution.Deconvert("ab"));
 
 public static class Solution
 {
@@ -21,7 +25,7 @@ public static class Solution
             if (firstGroup != item)
             {
                 if(count > 1)
-                    result.Append(firstGroup + count.ToString());
+                    result.Append(firstGroup).Append(count);
                 else
                     result.Append(firstGroup);
 
@@ -32,7 +36,25 @@ public static class Solution
             count++;
         }
         
-        return result.Append(firstGroup + (count != 1 ? count.ToString() : "")).ToString();
+        return result.Append(firstGroup).Append(count != 1 ? count.ToString() : "").ToString();
+    }
+    
+    public static string Deconvert(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return input;
+        
+        var result = new StringBuilder();
+        
+        foreach (var item in input)
+        {
+            if(char.IsLetter(item))
+                result.Append(item);
+            else if(char.IsDigit(item))
+                result.Append(new string(result[^1],int.Parse(item.ToString()) - 1));
+        }
+
+        return result.ToString();
     }
 }
 
