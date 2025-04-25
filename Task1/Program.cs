514db70b4b39faa530cf2264b22a0260f0464814
MinoddeinEz <kamazitun.jaykehd@gmail.com>
2025-04-25 22:54:59 +0300

    feat(task1): init, added convert method

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+Console.WriteLine(Solution.Convert("aaabbcccdde"));
+Console.WriteLine(Solution.Convert("aaaaaa"));
+Console.WriteLine(Solution.Convert("aaabbbbb"));
+Console.WriteLine(Solution.Convert("ab"));
+
+public static class Solution
+{
+    public static string Convert(string input)
+    {
+        if (string.IsNullOrWhiteSpace(input))
+            return input;
+        
+        var result = new StringBuilder();
+        var count = 0;
+        var firstGroup = input[0];
+        
+        foreach (var item in input)
+        {
+            if (firstGroup != item)
+            {
+                if(count > 1)
+                    result.Append(firstGroup + count.ToString());
+                else
+                    result.Append(firstGroup);
+
+                firstGroup = item;
+                count = 0;
+            }
+            
+            count++;
+        }
+        
+        return result.Append(firstGroup + (count != 1 ? count.ToString() : "")).ToString();
+    }
+}
+
