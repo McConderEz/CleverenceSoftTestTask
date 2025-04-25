68dae4f3a12aca21314176ae20a1c108f5c5ca6a
MinoddeinEz <kamazitun.jaykehd@gmail.com>
2025-04-26 02:39:46 +0300

    feat(task2): init project

---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using Task2;
+
+var tasks = new Task[10];
+
+for (var i = 0; i < 7; i++)
+{
+    tasks[i] = Task.Run(() =>
+    {
+        var random = new Random();
+        for (var j = 0; j < 5; j++) 
+        {
+            var count = Server.GetCount();
+            Console.WriteLine($"Читатель {Task.CurrentId} значение: {count}");
+            Thread.Sleep(random.Next(100, 500));
+        }
+    });
+}
+
+for (var i = 7; i < 10; i++)
+{
+    tasks[i] = Task.Run(() =>
+    {
+        var random = new Random();
+        for (var j = 0; j < 3; j++)
+        {
+            var valueToAdd = random.Next(1, 10);
+            Server.AddToCount(valueToAdd);
+            Console.WriteLine($"Писатель {Task.CurrentId} добавил {valueToAdd}, новое значение: {Server.GetCount()}");
+            Thread.Sleep(random.Next(200, 600)); 
+        }
+    });
+}
+
+await Task.WhenAll(tasks);
+
+Console.WriteLine($"Итоговое значение: {Server.GetCount()}");
---
+++ Task2/Server.cs
@@ -0,0 +1,39 @@
+﻿namespace Task2;
+
+public class ImmutableWrapper(int value)
+{
+    public int Value { get; } = value;
+}
+
+public static class Server
+{
+    private static ImmutableWrapper _value = new(10);
+    private static readonly ReaderWriterLockSlim  _lock = new();
+
+    public static int GetCount()
+    {
+        _lock.EnterReadLock();
+        try
+        {
+            return _value.Value;
+        }
+        finally
+        {
+            _lock.ExitReadLock();
+        }
+    }
+
+    public static void AddToCount(int value)
+    {
+        _lock.EnterWriteLock();
+        try
+        {
+            _value = new ImmutableWrapper(_value.Value + value);
+        }
+        finally
+        {
+            _lock.ExitWriteLock();
+        }
+    }
+    
+}
